931982717f91c5048880952d0a158c4ad940ab9a
andreclerigo <andreclerigo@outlook.com>
2024-01-10 17:56:41 +0000

    ground station parsing from lora and posting to the api

--- main/ground-station.c
+++ main/ground-station.c
@@ -7,6 +7,7 @@
 #include "cJSON.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "ground-station.h"
 #include "lora.h"
 
 
@@ -15,21 +16,11 @@
 #define WIFI_PASS CONFIG_WIFI_PASS
 #define SERVER_IP_ADDR CONFIG_SERVER_IP_ADDR
 
-// Data structure to send to the server
-typedef struct
-{
-    float acceleration_x;
-    float acceleration_y;
-    float acceleration_z;
-    float gyroscope_x;
-    float gyroscope_y;
-    float gyroscope_z;
-} data_t;
-
 // Tag for logging
 static const char *TAG = "GROUND_STATION";
 static char ip_addr_str[16] = {0};
 data_t *data_to_send = NULL;
+char *endpoints[2] = {"mpu6500", "neo7m"};
 
 // Event handlers
 static void on_got_ip(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
@@ -74,10 +65,6 @@ static void wifi_init(void)
     esp_wifi_start();
     esp_wifi_connect();
 
-    // DEBUG SSID AND PASSWORD
-    ESP_LOGI(TAG, "Connecting to %s...", WIFI_SSID);
-    ESP_LOGI(TAG, "Password: %s", WIFI_PASS);
-
     // Register the IP_EVENT_STA_GOT_IP event handler
     esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, on_got_ip, NULL);
 
@@ -105,71 +92,97 @@ static void wifi_init(void)
 
 void send_data(void *pvParameters)
 {
-    esp_http_client_config_t config = {
-        .url = "http://" SERVER_IP_ADDR ":3000/api/v1/mpu6500"};
-
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    // Set the HTTP method to POSTs
-    esp_http_client_set_method(client, HTTP_METHOD_POST);
-
-    // Create a JSON object
-    cJSON *root = cJSON_CreateObject();
-    cJSON *dataObject = cJSON_CreateObject();
-
-    // Add temperature and humidity to the object
-    cJSON_AddNumberToObject(dataObject, "acceleration_x", data_to_send->acceleration_x);
-    cJSON_AddNumberToObject(dataObject, "acceleration_y", data_to_send->acceleration_y);
-    cJSON_AddNumberToObject(dataObject, "acceleration_z", data_to_send->acceleration_z);
-    cJSON_AddNumberToObject(dataObject, "gyroscope_x", data_to_send->gyroscope_x);
-    cJSON_AddNumberToObject(dataObject, "gyroscope_y", data_to_send->gyroscope_y);
-    cJSON_AddNumberToObject(dataObject, "gyroscope_z", data_to_send->gyroscope_z);
-
-    // Add the data object to root
-    cJSON_AddItemToObject(root, "data", dataObject);
-
-    // Convert the JSON object to a string
-    char *json_string = cJSON_Print(root);
-    if (json_string == NULL)
+    for (int i = 0; i < sizeof(endpoints)/sizeof(endpoints[0]); i++) 
     {
-        ESP_LOGE(TAG, "Failed to print JSON");
-    }
-    else
-    {
-        esp_http_client_set_header(client, "Content-Type", "application/json");
-        // Set the JSON string as the body of the POST request
-        esp_http_client_set_post_field(client, json_string, strlen(json_string));
+        char url[256];
+        sprintf(url, "http://%s:3000/api/v1/%s", SERVER_IP_ADDR, endpoints[i]);
 
-        // Perform the HTTP POST request
-        esp_err_t err = esp_http_client_perform(client);
+        esp_http_client_config_t config = {
+            .url = url
+        };
 
-        if (err == ESP_OK)
+        esp_http_client_handle_t client = esp_http_client_init(&config);
+        // Set the HTTP method to POSTs
+        esp_http_client_set_method(client, HTTP_METHOD_POST);
+
+        // Create a JSON object
+        cJSON *root = cJSON_CreateObject();
+        cJSON *dataObject = cJSON_CreateObject();
+
+        if (strcmp(endpoints[i], "mpu6500") == 0)
         {
-            ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %lld",
-                     esp_http_client_get_status_code(client),
-                     esp_http_client_get_content_length(client));
+            // Add mpu attributes to the object
+            cJSON_AddNumberToObject(dataObject, "acceleration_x", data_to_send->acceleration_x);
+            cJSON_AddNumberToObject(dataObject, "acceleration_y", data_to_send->acceleration_y);
+            cJSON_AddNumberToObject(dataObject, "acceleration_z", data_to_send->acceleration_z);
+            cJSON_AddNumberToObject(dataObject, "gyroscope_x", data_to_send->gyroscope_x);
+            cJSON_AddNumberToObject(dataObject, "gyroscope_y", data_to_send->gyroscope_y);
+            cJSON_AddNumberToObject(dataObject, "gyroscope_z", data_to_send->gyroscope_z);
+        }
+        else if (strcmp(endpoints[i], "neo7m") == 0)
+        {
+            // Add neo7m attributes to the object
+            cJSON_AddNumberToObject(dataObject, "latitude", data_to_send->latitude);
+            cJSON_AddNumberToObject(dataObject, "longitude", data_to_send->longitude);
+            cJSON_AddNumberToObject(dataObject, "speed", data_to_send->speed);
+            // Create time in format HH:MM:SS using 3 values data->hours, data->minutes, data->seconds
+            char time[12];
+            sprintf(time, "%02d:%02d:%02d", data_to_send->hours, data_to_send->minutes, data_to_send->seconds);
+            cJSON_AddStringToObject(dataObject, "time", time);
+
+            // Create date in format DD-MM-YYYY using 3 values data->day, data->month, data->year
+            char date[12];
+            sprintf(date, "%02d-%02d-%02d", data_to_send->day, data_to_send->month, data_to_send->year);
+            cJSON_AddStringToObject(dataObject, "date", date);
+        }
+
+        // Add the data object to root
+        cJSON_AddItemToObject(root, "data", dataObject);
+
+        // Convert the JSON object to a string
+        char *json_string = cJSON_Print(root);
+        if (json_string == NULL)
+        {
+            ESP_LOGE(TAG, "Failed to print JSON");
         }
         else
         {
-            ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+            esp_http_client_set_header(client, "Content-Type", "application/json");
+            // Set the JSON string as the body of the POST request
+            esp_http_client_set_post_field(client, json_string, strlen(json_string));
+
+            // Perform the HTTP POST request
+            esp_err_t err = esp_http_client_perform(client);
+
+            if (err == ESP_OK)
+            {
+                ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %lld",
+                        esp_http_client_get_status_code(client),
+                        esp_http_client_get_content_length(client));
+            }
+            else
+            {
+                ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+            }
+
+            // Clean up
+            free(json_string);
         }
 
-        // Clean up
-        free(json_string);
+        // Delete the JSON object
+        cJSON_Delete(root);
+        // Cleanup
+        esp_http_client_cleanup(client);
     }
 
-    // Delete the JSON object
-    cJSON_Delete(root);
-    // Cleanup
-    esp_http_client_cleanup(client);
     vTaskDelete(NULL);
 }
-
 
 
 void app_main()
 {
     data_to_send = malloc(sizeof(data_t));
-    char *data = malloc(1000);
+    char *data = malloc(sizeof(data_t));
 
     // Initialize NVS, required for Wi-Fi
     esp_err_t ret = nvs_flash_init();
@@ -195,18 +208,22 @@ void app_main()
         if (ret_uart == ESP_OK)
         {
             // Parse the received data and assign it to data_to_send
-            sscanf(data, "%*d: %f,%f,%f,%f,%f,%f",
+            sscanf(data, "%*d: %f,%f,%f,%f,%f,%f,%f,%f,%f,%hhu,%hhu,%hhu,%hhu,%hhu,%hhu",
                    &data_to_send->acceleration_x,
                    &data_to_send->acceleration_y,
                    &data_to_send->acceleration_z,
                    &data_to_send->gyroscope_x,
                    &data_to_send->gyroscope_y,
-                   &data_to_send->gyroscope_z);
-            
-            // DEBUG GYROSCOPE DATA
-            ESP_LOGI(TAG, "Gyroscope X: %f", data_to_send->gyroscope_x);
-            ESP_LOGI(TAG, "Gyroscope Y: %f", data_to_send->gyroscope_y);
-            ESP_LOGI(TAG, "Gyroscope Z: %f", data_to_send->gyroscope_z);
+                   &data_to_send->gyroscope_z,
+                   &data_to_send->latitude,
+                   &data_to_send->longitude,
+                   &data_to_send->speed,
+                   &data_to_send->day,
+                   &data_to_send->month,
+                   &data_to_send->year,
+                   &data_to_send->hours,
+                   &data_to_send->minutes,
+                   &data_to_send->seconds);
 
             // Create a task to send data to the server
             xTaskCreate(send_data, "send_data", 4096, NULL, 1, NULL);
---
+++ main/ground-station.h
@@ -0,0 +1,18 @@
+typedef struct
+{
+    float acceleration_x;
+    float acceleration_y;
+    float acceleration_z;
+    float gyroscope_x;
+    float gyroscope_y;
+    float gyroscope_z;
+    float latitude;
+    float longitude;
+    float speed;
+    uint8_t day;
+    uint8_t month;
+    uint8_t year;
+    uint8_t hours;
+    uint8_t minutes;
+    uint8_t seconds;
+} data_t;
